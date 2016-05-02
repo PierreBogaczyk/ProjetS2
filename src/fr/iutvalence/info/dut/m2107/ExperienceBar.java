3a2936285e113a761e600f0564ba49e92892c9ef
delarbrm <delarbrm@PC-B102-03.iut-valence.fr>
2016-05-02 15:47:43 +0200

    set constructor and method for characteristic and ExperienceBar

--- src/fr/iutvalence/info/dut/m2107/ExperienceBar.java
+++ src/fr/iutvalence/info/dut/m2107/ExperienceBar.java
@@ -4,14 +4,32 @@ public class ExperienceBar {
 	//TODO add attributes and methods 
 	private int currentExperienceAmount;
 	private int maxExperienceAmount;
+	private static final int Empty_currentExperienceAmount=0;
 	
 	public void emptyBar()
 	{
-		
+		this.currentExperienceAmount=Empty_currentExperienceAmount;
 	}
 	
-	public void addExperience()
+	public ExperienceBar(int currentExperienceAmount, int maxExperienceAmount)
 	{
-		
+		this.currentExperienceAmount = currentExperienceAmount;
+		this.maxExperienceAmount = maxExperienceAmount;
 	}
+
+	public void addExperience(int Experience)
+	{
+		this.currentExperienceAmount=this.currentExperienceAmount+Experience;
+	}
+	
+	public void Levelup(Character player){
+		if(this.currentExperienceAmount>=this.maxExperienceAmount){
+			this.currentExperienceAmount=Empty_currentExperienceAmount+(this.maxExperienceAmount-this.currentExperienceAmount);
+			player.setCharacterLevel(getCharacterLevel() + 1);			
+		}
+		
+	
+	}
+	
+	
 }
