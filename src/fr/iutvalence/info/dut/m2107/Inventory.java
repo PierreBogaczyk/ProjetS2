c1c75176268698106b93fb79e2ecca2ce35bffd1
godartq <godartq@PC-B123-06.iut-valence.fr>
2016-05-04 08:54:48 +0200

    Item and Inventory modifications

--- src/fr/iutvalence/info/dut/m2107/Inventory.java
+++ src/fr/iutvalence/info/dut/m2107/Inventory.java
@@ -5,4 +5,25 @@ public class Inventory {
 	//TODO add attributes and methods
 	private int inventorySlotsCount;
 	private ArrayList<Item> itemInInventory;
+	
+	/**
+	 * Constructor of Inventory
+	 */
+	public Inventory () {
+		
+		
+		
+	}
+	
+	/**
+	 * Allow to throw item
+	 */
+	public void throwItem () {
+		
+		
+		
+	}
+	
+	
+	
 }
--- src/fr/iutvalence/info/dut/m2107/Item.java
+++ src/fr/iutvalence/info/dut/m2107/Item.java
@@ -3,7 +3,53 @@ import java.util.ArrayList;
 
 public class Item {
 	//TODO add attributes and methods 
+	/**
+	 * Type of item
+	 */
 	private String itemType;
+	/**
+	 * Value of item
+	 */
 	private int itemValue;
-	private ArrayList<Characteristic> itemCharacteristc;
+	/**
+	 * I don't know
+	 */
+	private ArrayList<Characteristic> itemCharacteristic;
+	/**
+	 * Id of item
+	 */
+	private int idItem;
+	
+	/**
+	 * This is the constructor of Item
+	 * @param idItem identificator of item 
+	 * @param itemType type of item
+	 * @param itemValue value of item
+	 */
+	public Item (int idItem, String itemType, int itemValue) {
+		
+		this.idItem=idItem;
+		this.itemType=itemType;
+		this.itemValue=itemValue;
+		
+	}
+	
+	/**
+	 * Allow to equip the item	 
+	 */
+	public void equipItem () {
+		
+		
+		
+	}
+	
+	/**
+	 * Allow to use the item
+	 */
+	public void useItem () {
+		
+		
+		
+	}
+	
 }
