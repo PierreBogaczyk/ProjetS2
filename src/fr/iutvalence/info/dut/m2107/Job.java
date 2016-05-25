896505cfd9cd6bf0867a55d572afa27cab99e6ef
godartq <godartq@PC-B102-01.iut-valence.fr>
2016-05-25 10:55:39 +0200

    Job

--- src/fr/iutvalence/info/dut/m2107/Job.java
+++ src/fr/iutvalence/info/dut/m2107/Job.java
@@ -1,4 +1,10 @@
 package fr.iutvalence.info.dut.m2107;
+import fr.iutvalence.info.dut.m2107.action.Skill;
+import fr.iutvalence.info.dut.m2107.action.Attack;
+import fr.iutvalence.info.dut.m2107.action.DamageSpell;
+import fr.iutvalence.info.dut.m2107.action.HealingSpell;
+import java.util.ArrayList;
+
 /**
  * create class job
  * @author delarbrm
@@ -10,6 +16,7 @@ public class Job {
 	 * job attribute
 	 */
 	public String jobName;
+	private ArrayList<Skill> jobSkill;
 	/**
 	 * job by default
 	 */
@@ -17,15 +24,31 @@ public class Job {
 /**
  * allow a job
  */
-	public Job(){
-		this.jobName=DEFAULT_JOB;
-		}
-	/**
-	 * allow a define Job
-	 * @param Job
-	 */
 	public Job(String jobName){
-		this.jobName=jobName;
+		
+		this.jobName=DEFAULT_JOB;
+		this.jobSkill=null;
+		
+			switch (jobName){
+				
+				case "Warrior" :
+					jobSkill.add(new Attack ("Head breaker",20));
+					jobSkill.add(new Attack ("Slice",15));
+					jobSkill.add(new Attack ("Cut",10));
+					break;
+					
+				case "Rogue" :
+					jobSkill.add(new Attack ("Flame arrow",15));
+					jobSkill.add(new Attack ("Clean shot",20));
+					jobSkill.add(new Attack ("Vital point",30));
+					break;
+					
+				case "Wizard"	:
+					jobSkill.add(new DamageSpell ("Ice gun",15));
+					jobSkill.add(new HealingSpell ("Health giver",15));
+					jobSkill.add(new Attack ("Lightning strike",30));
+					break;
+			} 
 	}
 	
 	public String getJobName()
